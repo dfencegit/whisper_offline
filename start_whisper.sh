ba13c0866faa58dd031e35847b7d053b5d69834e
root <root@discoupdate.imferqype0metjzazmtblmjcpf.zx.internal.cloudapp.net>
2023-05-31 17:46:07 +0000

    Updated start script

--- start_whisper.sh
+++ start_whisper.sh
@@ -3,6 +3,6 @@
 docker stop whisper
 docker rm whisper
 
-docker run -d -p 9000:9000 -e ASR_MODEL=large --name whisper -v /cogserv/whisper:/root  onerahmet/openai-whisper-asr-webservice:latest
+docker run -d -p 9000:9000 -e HF_DATASETS_OFFLINE=1 -e TRANSFORMERS_OFFLINE=1 -e ASR_MODEL="/root/.cache/whisper/large-v1.pt" --name whisper -v /cogserv/whisper/230504/models/whisper:/root/.cache/whisper quay.hmi.twks/openai/whisper_offline:23050402
 
 docker logs --tail=100 -f whisper
